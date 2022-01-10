fc29461fcd6ffed8e49a5a4cd5c50bef8a770b0b
Maxime972 <67092377+Maxime972@users.noreply.github.com>
2022-01-10 17:46:07 +0100

    modif parallaxe

--- js/parallax.js
+++ js/parallax.js
@@ -2,5 +2,10 @@ const parallax = document.querySelector('.parallax');
 
 window.addEventListener('scroll', () => {
     //console.log(window.scrollY/2);
-    parallax.style.backgroundPositionY = -window.scrollY / 4 + "px";
+    if (window.screen.width < 768)
+    {
+        parallax.style.backgroundPositionY = -window.scrollY / 4 + 'px'
+    }else {
+        parallax.style.backgroundPositionY = -window.scrollY / 2 + 'px'
+    }
 });
