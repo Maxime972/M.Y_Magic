30c54f96d2b5f5bbd972b2802c5f6cc83090b43c
Maxime972 <67092377+Maxime972@users.noreply.github.com>
2022-01-10 17:19:41 +0100

    modification effet parallaxe

--- js/parallax.js
+++ js/parallax.js
@@ -2,5 +2,5 @@ const parallax = document.querySelector('.parallax');
 
 window.addEventListener('scroll', () => {
     //console.log(window.scrollY/2);
-    parallax.style.backgroundPositionY = -window.scrollY / 2 + "px";
+    parallax.style.backgroundPositionY = -window.scrollY / 4 + "px";
 });
